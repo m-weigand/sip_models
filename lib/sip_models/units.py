2f417591c9000ffb86f9ac29c1a5dcf5da901358
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2018-01-29 10:13:55 +0100

    add a units module

---
+++ lib/sip_models/units.py
@@ -0,0 +1,44 @@
+# define various labels/units for common SIP parameters
+
+labels = {
+    'rmag': {
+        'material': {
+            'latex': r'$|\rho|~[\Omega m]$',
+            'mathml': r'$|\rho| [\Omega m]$',
+        },
+        'meas': {
+            'latex': r'$|Z|~[\Omega]$',
+            'mathml': r'$|Z| [\Omega]$',
+        },
+    },
+    'rpha': {
+        'material': {
+            'latex': r'$-\phi~[mrad]$',
+            'mathml': r'$-\phi [mrad]$',
+        },
+        'meas': {
+            'latex': r'$-\varphi~[mrad]$',
+            'mathml': r'$-\varphi [mrad]$',
+        },
+    },
+    'cre': {
+        'material': {
+            'latex': r"$\sigma'~[S/m]$",
+            'mathml': r"$\sigma' [S/m]$",
+        },
+        'meas': {
+            'latex': r"$\Y'~[S]$",
+            'mathml': r"$\Y' [S]$",
+        },
+    },
+    'cim': {
+        'material': {
+            'latex': r"$\sigma''~[S/m]$",
+            'mathml': r"$\sigma'' [S/m]$",
+        },
+        'meas': {
+            'latex': r"$\Y''~[S]$",
+            'mathml': r"$\Y'' [S]$",
+        },
+    },
+}
